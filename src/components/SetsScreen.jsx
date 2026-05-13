8d53459dedf9b2887151e8e3f293fadd4ec58d86
sophiepavlova <olni.pavlova@gmail.com>
2026-05-13 12:11:40 -0700

    feat: add All to the sets

--- src/components/SetsScreen.jsx
+++ src/components/SetsScreen.jsx
@@ -2,11 +2,23 @@ import StudyIntro from './StudyIntro';
 import MenuList from './MenuList';
 
 export default function SetsScreen({ sets, selectedDeck, onSelectSet }) {
-  console.log(selectedDeck);
+  const totalCards = sets.reduce((sum, set) => {
+    return sum + set.cardCount;
+  }, 0);
+
+  const allSet = {
+    id: 'all-sets',
+    title: 'All',
+    cardCount: totalCards,
+    meta: 'All cards in this deck',
+  };
+
+  const setsWithAll = [...sets, allSet];
+
   return (
     <div className='container'>
       <StudyIntro selectedDeck={selectedDeck} />
-      <MenuList items={sets} onSelectItem={onSelectSet} />
+      <MenuList items={setsWithAll} onSelectItem={onSelectSet} />
     </div>
   );
 }
