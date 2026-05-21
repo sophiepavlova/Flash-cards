7d67f21e0b5dedba9588bac1ba96a87107aa2b92
sophiepavlova <olni.pavlova@gmail.com>
2026-05-21 15:26:28 -0700

    Style: cleaned up the spacings in the header, moved the buttons on the Results screen

--- src/components/ResultsScreen.jsx
+++ src/components/ResultsScreen.jsx
@@ -54,51 +54,56 @@ export default function ResultsScreen({
           />
         </div>
         <h2 className='intro-heading'>Session is complete</h2>
-        <div className='diagram'>
-          <PieChart width={386} height={386}>
-            <Pie
-              data={data}
-              dataKey='value'
-              innerRadius={70}
-              outerRadius={150}
-              stroke='none'
-            >
-              {data.map((entry, index) => (
-                <Cell key={index} fill={COLORS[index]} />
-              ))}
-            </Pie>
-          </PieChart>
-          <div>
-            <div className='percents'>
-              <div className='percents-left'>
-                <div className='cirlce knew-it'></div>
-                <p className='percents-text'>Knew it</p>
+        <div className='results-body'>
+          <div className='diagram'>
+            <PieChart width={386} height={386}>
+              <Pie
+                data={data}
+                dataKey='value'
+                innerRadius={70}
+                outerRadius={150}
+                stroke='none'
+              >
+                {data.map((entry, index) => (
+                  <Cell key={index} fill={COLORS[index]} />
+                ))}
+              </Pie>
+            </PieChart>
+            <div>
+              <div className='percents'>
+                <div className='percents-left'>
+                  <div className='cirlce knew-it'></div>
+                  <p className='percents-text'>Knew it</p>
+                </div>
+                <div className='percent-value'>{knewItPercent || 0} %</div>
               </div>
-              <div className='percent-value'>{knewItPercent || 0} %</div>
-            </div>
-            <div className='percents'>
-              <div className='percents-left'>
-                <div className='cirlce unsure'></div>
-                <p className='percents-text'>Unsure</p>
+              <div className='percents'>
+                <div className='percents-left'>
+                  <div className='cirlce unsure'></div>
+                  <p className='percents-text'>Unsure</p>
+                </div>
+                <div className='percent-value'>{unsurePercent || 0} %</div>
               </div>
-              <div className='percent-value'>{unsurePercent || 0} %</div>
-            </div>
-            <div className='percents'>
-              <div className='percents-left'>
-                <div className='cirlce no-idea'></div>
-                <p className='percents-text'>No Idea</p>
+              <div className='percents'>
+                <div className='percents-left'>
+                  <div className='cirlce no-idea'></div>
+                  <p className='percents-text'>No Idea</p>
+                </div>
+                <div className='percent-value'>{noIdeaPercent || 0} %</div>
               </div>
-              <div className='percent-value'>{noIdeaPercent || 0} %</div>
             </div>
           </div>
-        </div>
-        <div className='results-btns'>
-          <button className='results-btn btn' onClick={handleStudyAgain}>
-            Study again
-          </button>
-          <button className='results-btn btn' onClick={handleChooseAnotherSet}>
-            Choose another set
-          </button>
+          <div className='results-btns'>
+            <button className='results-btn btn' onClick={handleStudyAgain}>
+              Study again
+            </button>
+            <button
+              className='results-btn btn'
+              onClick={handleChooseAnotherSet}
+            >
+              Choose another set
+            </button>
+          </div>
         </div>
       </div>
     </div>
