6af6b7bc83f4f373d0e42f969dfd8bc54c04ecd0
sophiepavlova <olni.pavlova@gmail.com>
2026-05-21 10:30:05 -0700

    feat: implement core flashcard study flow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ export default function Header({ onGoHome }) {
   return (
     <div className='container-header'>
       <div className='header-wrapper'>
-        <img src='./src/assets/Aloy_sticker 1.png' onClick={onGoHome}></img>
+        <img
+          className='header-img'
+          src='./src/assets/paddington.webp'
+          onClick={onGoHome}
+        ></img>
+        {/* <img src='./src/assets/Aloy_sticker 1.png' onClick={onGoHome}></img> */}
         <p className='accent-font logo-font' onClick={onGoHome}>
           NewHorizons
         </p>
